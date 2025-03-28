6d8d2c5b7b052b900fa3a7507c93b8315e97b433
Sokmontrey <sythatsokmontrey@gmail.com>
2025-03-28 10:42:10 -0400

    feat: human player move

--- src/Player/HumanPlayer.java
+++ src/Player/HumanPlayer.java
@@ -4,6 +4,8 @@ import Game.Game;
 import Game.CellPos;
 import Utils.Input;
 
+import java.util.Scanner;
+
 public class HumanPlayer extends Player{
     public HumanPlayer(char[] availableSymbols) {
         super(availableSymbols);
@@ -21,9 +23,27 @@ public class HumanPlayer extends Player{
         return availableSymbols[choice - 1];
     }
 
+    private CellPos readMove(Scanner scanner) {
+        System.out.print("Enter your row: ");
+        int row = scanner.nextInt();
+        System.out.print("Enter your column: ");
+        int col = scanner.nextInt();
+        System.out.println();
+        return new CellPos(row, col);
+    }
+
     @Override
     public CellPos makeMove(Game game) {
-        // TODO read player input and return the move
-        return new CellPos(0, 0);
+        Scanner scanner = new Scanner(System.in);
+        CellPos pos = readMove(scanner);
+        while (!game.isCorrectMove(pos)) {
+            System.out.println("Invalid move, try again");
+            pos = readMove(scanner);
+        }
+        while (!game.isCellEmpty(pos)) {
+            System.out.println("Cell is not empty, try again");
+            pos = readMove(scanner);
+        }
+        return pos;
     }
 }
