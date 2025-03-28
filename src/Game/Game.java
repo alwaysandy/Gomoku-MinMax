891bbaaefe9946bad32a401acf01b38c9255774b
Sokmontrey <sythatsokmontrey@gmail.com>
2025-03-28 11:42:34 -0400

    feat: minimax TODO: test

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -1,14 +1,17 @@
 package Game;
 
+import java.util.function.Function;
+
 public class Game {
     private char[][] board;
+    private int size;
 
-    public Game() {
-        board = new char[9][9];
+    public Game(int size) {
+        board = new char[size][size];
     }
 
     public boolean setCell(CellPos pos, char symbol) {
-        if (pos.row < 0 || pos.row > 8 || pos.col < 0 || pos.col > 8) return false;
+        if (pos.row < 0 || pos.row >= size || pos.col < 0 || pos.col >= size) return false;
         if (board[pos.row][pos.col] != 0) return false;
         board[pos.row][pos.col] = symbol;
         return true;
@@ -23,14 +26,14 @@ public class Game {
         int leftDiag = 1;
         int rightDiag = 1;
 
-        horizontal += countInDirection(row, col, 1, 0, 9, 10, color);
+        horizontal += countInDirection(row, col, 1, 0, size, 10, color);
         horizontal += countInDirection(row, col, -1, 0, -1, 10, color);
-        vertical += countInDirection(row, col,0, 1, 10, 9, color);
+        vertical += countInDirection(row, col, 0, 1, 10, size, color);
         vertical += countInDirection(row, col, 0, -1, 10, -1, color);
         leftDiag += countInDirection(row, col, -1, -1, -1, -1, color);
-        leftDiag += countInDirection(row, col, 1, 1, 9, 9, color);
-        rightDiag += countInDirection(row, col, -1, 1, -1, 9, color);
-        rightDiag += countInDirection(row, col, 1, -1, 9, -1, color);
+        leftDiag += countInDirection(row, col, 1, 1, size, size, color);
+        rightDiag += countInDirection(row, col, -1, 1, -1, size, color);
+        rightDiag += countInDirection(row, col, 1, -1, size, -1, color);
 
         return (horizontal == 5 || vertical == 5 || leftDiag == 5 || rightDiag == 5);
     }
@@ -57,18 +60,16 @@ public class Game {
     @Override
     public String toString() {
         StringBuilder boardString = new StringBuilder();
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 if (board[i][j] != 'W' && board[i][j] != 'B') {
                     boardString.append('.');
                 } else {
                     boardString.append(board[i][j]);
                 }
             }
-
             boardString.append('\n');
         }
-
         return boardString.toString();
     }
 
@@ -78,12 +79,26 @@ public class Game {
 
     public boolean isCorrectMove(CellPos pos) {
         return pos.row >= 0 &&
-                pos.row < 9 &&
+                pos.row < size &&
                 pos.col >= 0 &&
-                pos.col < 9;
+                pos.col < size;
     }
 
     public boolean isCellEmpty(CellPos pos) {
         return board[pos.row][pos.col] == 0;
+    }
+
+    public Game copy() {
+        Game copy = new Game(size);
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                copy.setCell(new CellPos(i, j), board[i][j]);
+            }
+        }
+        return copy;
+    }
+
+    public int getSize() {
+        return size;
     }
 }
