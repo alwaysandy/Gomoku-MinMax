6d0665901b051fc6aa45bc3c855c1299fd6b26a7
Sokmontrey <sythatsokmontrey@gmail.com>
2025-04-04 11:16:09 -0400

    feat: use alphabetic char for cols

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -79,7 +79,7 @@ public class Game {
         StringBuilder boardString = new StringBuilder();
         boardString.append("  ");
         for (int i = 0; i < size; i++) {
-            boardString.append(i + " ");
+            boardString.append((char)(i + 'A') + " ");
         }
         boardString.append('\n');
         for (int i = 0; i < size; i++) {
